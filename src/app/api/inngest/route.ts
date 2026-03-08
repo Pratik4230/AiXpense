ce44956e21043a8505c2d1f7cb369ae96f737871
Pratik4230 <pratikjadhav1438@gmail.com>
2026-03-08 18:35:24 +0530

    inngest

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest, functions } from "@/inngest/index";
 
+export const maxDuration = 60;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions,
