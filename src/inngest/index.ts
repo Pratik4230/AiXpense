72c51eaceca59634e4187334bb46217df6261f47
Pratik4230 <pratikjadhav1438@gmail.com>
2026-03-08 19:06:44 +0530

    Inngest test removed

--- src/inngest/index.ts
+++ src/inngest/index.ts
@@ -2,7 +2,6 @@ import { inngest } from "@/inngest/client";
 import { onboardingDrip } from "./onboardingDrip";
 import { aiCoachWeekly, aiCoachMonthly } from "./aiCoach";
 import { cleanupUnverified } from "./cleanupUnverified";
-import { testEmail } from "./testEmail";
 
-export const functions = [onboardingDrip, aiCoachWeekly, aiCoachMonthly, cleanupUnverified, testEmail];
+export const functions = [onboardingDrip, aiCoachWeekly, aiCoachMonthly, cleanupUnverified];
 export { inngest };
