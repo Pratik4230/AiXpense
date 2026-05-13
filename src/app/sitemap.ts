bb59e5ccbfe2d4276ef0d9226dca0d572b1b8c67
Pratik4230 <pratikjadhav1438@gmail.com>
2026-05-13 15:18:21 +0530

    removed Blogs

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,8 @@
 import { MetadataRoute } from "next";
-import { getAllPosts } from "@/lib/blog";
 
 const BASE_URL = "https://aixpense.in";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const posts = getAllPosts();
-
-  const blogUrls: MetadataRoute.Sitemap = posts.map((post) => ({
-    url: `${BASE_URL}/blog/${post.slug}`,
-    lastModified: new Date(post.publishedAt),
-    changeFrequency: "monthly",
-    priority: 0.7,
-  }));
-
   return [
     {
       url: BASE_URL,
@@ -20,13 +10,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-    {
-      url: `${BASE_URL}/blog`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-    ...blogUrls,
     {
       url: `${BASE_URL}/login`,
       lastModified: new Date(),
